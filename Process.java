8afaa34acbbff50a2039956487c0b41df825c666
Malakhassan8 <malakhassan2448@gmail.com>
2025-12-13 18:01:21 +0200

    Add Process class with scheduling attributes

---
+++ Process.java
@@ -0,0 +1,61 @@
+public class Process {
+
+    private String name;
+    private int arrivalTime;
+    private int burstTime;
+    private int remainingTime;
+    private int completionTime;
+    private int waitingTime;
+    private int turnaroundTime;
+
+    public Process(String name, int arrivalTime, int burstTime) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.remainingTime = burstTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+    }
+
+    public int getCompletionTime() {
+        return completionTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+}
